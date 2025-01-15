c65b7903d4de9fa2aeda7034da1529e2092b18b0
Endoxidev <113648540+MetaMysteries8@users.noreply.github.com>
2025-01-14 21:01:25 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,3 @@
+let mySprite = sprites.create(assets.image`EncodedLogo`, SpriteKind.Player)
+mySprite.scale = 4
+story.printText("Project Encoded Coming Soon... Keep an eye out for updates!", 75, 0, 15, 1, story.TextSpeed.VerySlow)
